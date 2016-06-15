499c77fb789c3039084341bf46424408038453a1
Alexander Krasnoschekov <akrasnoschekov@gmail.com>
2016-06-15 16:31:15 +0300

    a bit of refactoring

--- rusby/builder.rb
+++ rusby/builder.rb
@@ -13,22 +13,26 @@ module Rusby
       @rust ||= Hashie::Mash.new YAML.load_file("#{root_path}/rust.yaml")
     end
 
-    def convert_to_rust(name, orig_method, owner, result, *args)
-      ast = Parser::Ruby22.parse(orig_method.source)
-
-      signature, code = Builder.method_to_rust(ast, args.map(&:class), result.class)
-      Builder.inline_methods(code, owner)
-
+    def postprocess(code)
       # fold the array syntax
       code.gsub!(/(\w+) \[\] (\w+)/, '\1[\2]')
       code.gsub!(/(\w+) \[\]= (\w+) =/, '\1[\2] =')
+    end
+
+    def convert_to_rust(name, orig_method, owner, result, *args)
+      signature, code = main_method(
+        orig_method.source,
+        args.map(&:class),
+        result.class
+      )
+      expand_inline_methods(code, owner)
+      postprocess(code)
 
       File.open("#{root_path}/lib/#{name}.rs", 'w') { |file| file.write(code) }
       `rustfmt #{root_path}/lib/#{name}.rs`
       File.open("#{root_path}/lib/#{name}.rs") { |file| puts file.read }
-      nil
 
-      # puts "Compiling #{signature.last} #{name}(#{signature.first.join(', ')})..."
+      puts "Compiling #{signature.last} #{name}(#{signature.first.join(', ')})..."
       # puts `rustc --crate-type=dylib -O -o #{root_path}/lib/#{name}.dylib #{root_path}/lib/#{name}.rs`
 
       # Proxy.rusby_load "#{root_path}/lib/#{name}"
@@ -37,40 +41,39 @@ module Rusby
       # Proxy.method(name)
     end
 
-    def inline_methods(code, owner)
-      code.gsub!(/inline_method_(\w+)\([^\)]+\)/) do
-        source = owner.method(Regexp.last_match(1)).source
-        ast = Parser::Ruby22.parse(source)
-
-        result = ''
-        ast.children[2..-1].each do |node|
-          result += Rust.generate(node)
-        end
-
-        result
-      end
-    end
-
-    def method_to_rust(ast, arg_types, return_type)
-      result = rust.method_declaration_prefix
-
+    def rust_method_body(ast)
       name = ast.children.first
       Rust.set_locals(name)
 
+      result = ''
+      ast.children[2..-1].each do |node|
+        result += Rust.generate(node)
+      end
+      result
+    end
+
+    def expand_inline_methods(code, owner)
+      code.gsub!(/inline_method_(\w+)\([^\)]+\)/) do
+        source = owner.method(Regexp.last_match(1)).source
+        ast = Parser::Ruby22.parse(source)
+        rust_method_body(ast)
+      end
+    end
+
+    def main_method(source, arg_types, return_type)
+      result = rust.method_declaration_prefix + "\n"
+
+      ast = Parser::Ruby22.parse(source)
       args = ast.children[1].children.each_with_index.map do |child, i|
         "#{child.children[0]}: #{rust.types[arg_types[i]]}"
       end
 
-      result << "#{rust.method_prefix} #{name}(#{args.join(', ')}) -> #{rust.types[return_type]} {"
-
-      ast.children[2..-1].each do |node|
-        result << Rust.generate(node)
-      end
-
-      result << '}'
+      result += "#{rust.method_prefix} #{ast.children[0]}(#{args.join(', ')}) -> #{rust.types[return_type]} {"
+      result += rust_method_body(ast)
+      result += '}'
 
       signature = [arg_types.map { |arg| rust.c_types[arg] }, rust.c_types[return_type]]
-      [signature, result.join("\n")]
+      [signature, result]
     end
   end
 end
